359ace6f7648ed84cd2d5ee7be2cfe06ba0a409d
Bee <xronsredstone@gmail.com>
2023-02-02 18:41:11 -0500

    navbar

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -10,11 +10,40 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-red-500">Hello World</h1>
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">ShelterGame</a>
+          </div>
+          <div className="flex-none gap-2">
+            <div className="dropdown dropdown-end">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 h-10 p-1 bg-red-500 rounded-full">
+                  <p>B</p>
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <a className="justify-between">Profile</a>
+                </li>
+                <li>
+                  <a>Login</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </main>
       <footer className="fixed bottom-0 w-full">
-        <div className="items-center grid-flow-col">
-            <p className="text-center text-sm text-gray-500"> Made by <a href="GitHub.com/MadeOfBees" className="text-blue-500">MadeOfBees</a> on Github </p>
+        <div className="footer footer-center p-4 bg-base-300 text-base-content">
+          <a
+            href="http://www.github.com/MadeOfBees"
+            className="text-base-content"
+          >
+            This site is MadeOfBees
+          </a>
         </div>
       </footer>
     </>
